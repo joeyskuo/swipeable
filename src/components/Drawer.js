fc6fc016b81de531956511de3e69ab7ba4206989
Joey Kuo <joey.kuo@rugsusa.com>
2022-12-09 13:23:24 -0800

    added initial touch handlers

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,8 +1,36 @@
 import logo from '../logo.svg';
 
 const Drawer = () => {
+
+    const handleClick = () => {
+        console.log("click");
+    }
+
+    const handleTouchStart = (e) => {
+        console.log("touch start");
+        console.log(e.touches[0].clientY);
+    }
+
+    const handleTouchMove = () => {
+        console.log("touch move");
+    }
+
+    const handleTouchEnd = () => {
+        console.log("touch end");
+    }
+
+    const handleTouchCancel = () => {
+        console.log("touch cancel");
+    }
+
     return (
-        <div className="drawer swipeable">
+        <div className="drawer swipeable" 
+                onClick={() => handleClick()}
+                onTouchStart={(e) => handleTouchStart(e)}
+                onTouchMove={() => handleTouchMove()}
+                onTouchEnd={() => handleTouchEnd()}
+                onTouchCancel={() => handleTouchCancel()}
+                >
             <img src={logo} className="App-logo" alt="logo" />
         </div>
     )
