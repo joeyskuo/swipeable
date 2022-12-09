492559e10ecc234b8cef7ce8044f379b88cdeb91
Joey Kuo <joey.kuo@rugsusa.com>
2022-12-09 13:52:35 -0800

    added drawer container; added corresponding styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="container">
         <Drawer/>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
