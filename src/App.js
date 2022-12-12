2e8a008333c9deada6252891d5259fb86bf848d9
Joey Kuo <joey.kuo@rugsusa.com>
2022-12-12 10:53:14 -0800

    added button that toggles drawer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 import Drawer from './components/Drawer';
 
+
+const toggleDrawer = () => {
+  const drawer = document.querySelector('.drawer');
+  if(drawer.classList.contains('visible')) {
+    drawer.classList.remove('visible');
+  } else {
+    drawer.classList.add('visible');
+  }
+}
+
 function App() {
   return (
     <div className="App">
+      <button className="drawer-btn"
+              onClick={() => toggleDrawer()}
+      >
+        Toggle Drawer
+      </button>
       <header className="App-header">
         <div className="container">
         <Drawer/>
